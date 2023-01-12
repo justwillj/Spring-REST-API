811740d1a17f3a8ef5e581902f589eda3069023c
Justin Williams <williams.jman@gmail.com>
2023-01-11 23:00:24 -0500

    Added the getters and setters for the employee

--- src/main/java/edu/midlands/training/entities/Employee.java
+++ src/main/java/edu/midlands/training/entities/Employee.java
@@ -25,4 +25,45 @@ public class Employee {
     this.isActive = isActive;
   }
 
+  public Integer getEmployeeId() {
+    return employeeId;
+  }
+
+  public void setEmployeeId(Integer employeeId) {
+    this.employeeId = employeeId;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getAge() {
+    return age;
+  }
+
+  public void setAge(String age) {
+    this.age = age;
+  }
+
+  public boolean isActive() {
+    return isActive;
+  }
+
+  public void setActive(boolean active) {
+    isActive = active;
+  }
+
+
 }

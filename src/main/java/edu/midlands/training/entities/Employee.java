ce6888f32076ddf0fd48dcdcf9f864dddbf42215
Justin Williams <williams.jman@gmail.com>
2023-01-11 22:56:47 -0500

    Created the layout of the employee object

--- src/main/java/edu/midlands/training/entities/Employee.java
+++ src/main/java/edu/midlands/training/entities/Employee.java
@@ -2,4 +2,14 @@ package edu.midlands.training.entities;
 
 public class Employee {
 
+  private Integer employeeId;
+
+  private String firstName;
+
+  private String lastName;
+
+  private String age;
+
+  private boolean isActive;
+
 }
--- src/main/java/edu/midlands/training/services/EmployeeServiceImpl.java
+++ src/main/java/edu/midlands/training/services/EmployeeServiceImpl.java
@@ -3,7 +3,9 @@ package edu.midlands.training.services;
 import edu.midlands.training.entities.Employee;
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.stereotype.Service;
 
+@Service
 public class EmployeeServiceImpl implements EmployeeService {
 
   List<Employee> employeeList =new ArrayList<>();
